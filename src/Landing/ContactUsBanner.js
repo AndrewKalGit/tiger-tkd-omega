8b9da0f3b0b732a692a2d30329decca108929c19
Sungmin Lee <sungminlee417@gmail.com>
2023-06-17 16:05:36 -0700

    finishing up testimonial page

--- src/Landing/ContactUsBanner.js
+++ src/Landing/ContactUsBanner.js
@@ -1,50 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import kickMan from '../Images/TKDMAN1.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import kickMan from "../images/TKDMAN1.png";
 
 function ContactUsBanner(props) {
-    return (
-        <>
-            <section
-            className="vh-100 overflow-hidden bg-gray-50 sm:grid xl:grid-cols-2 sm:items-center"
-            >
-                <div class="p-8 md:p-12 lg:px-16 lg:py-24">
-                    <div
-                    class="vh-50 mx-auto max-w-2xl text-center ltr:sm:text-left rtl:sm:text-right"
-                    >
-                    <img class="mb-10" src={kickMan} alt="kickingMan"/>
-                    <h2 class="text-3xl xl:text-left font-bold text-gray-900 md:text-5xl">
-                        Lorem, ipsum dolor sit amet!
-                    </h2>
-                    <p class="mt-3 xl:text-left text-gray-700 md:mt-5 md:block">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper.
-                    </p>
-                
-                    <p class="mt-3 xl:text-left text-gray-700 md:mt-5 md:block">                        
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper.
-                    </p>
+  return (
+    <>
+      <section className="vh-100 overflow-hidden bg-gray-50 sm:grid xl:grid-cols-2 sm:items-center">
+        <div class="p-8 md:p-12 lg:px-16 lg:py-24">
+          <div class="vh-50 mx-auto max-w-2xl text-center ltr:sm:text-left rtl:sm:text-right">
+            <img class="mb-10" src={kickMan} alt="kickingMan" />
+            <h2 class="text-3xl xl:text-left font-bold text-gray-900 md:text-5xl">
+              Lorem, ipsum dolor sit amet!
+            </h2>
+            <p class="mt-3 xl:text-left text-gray-700 md:mt-5 md:block">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
+              egestas tempus tellus etiam sed. Quam a scelerisque amet
+              ullamcorper.
+            </p>
 
-                    <div class="mt-20 md:mt-30 flex justify-center">
-                        <Link
-                        to="/contact"
-                        class="rounded border-2 border-gray-900 bg-gray-50  px-12 py-3 text-sm font-medium text-gray-900 transition hover:bg-gray-50 focus:outline-none focus:ring focus:ring-blue-400"
-                        >
-                        Contact Us
-                        </Link>
-                    </div>
-                    </div>
-                </div>
+            <p class="mt-3 xl:text-left text-gray-700 md:mt-5 md:block">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
+              egestas tempus tellus etiam sed. Quam a scelerisque amet
+              ullamcorper.
+            </p>
 
-                <img
-                    alt=""
-                    src={''}
-                   className="bg-gray-400 object-cover mb-40 xl:mr-10 vh-75 xl:self-end xl:rounded-tl-[150px] xl:rounded-br-[150px]"
-                />
-            </section>   
-        </>
-    );
+            <div class="mt-20 md:mt-30 flex justify-center">
+              <Link
+                to="/contact"
+                class="rounded border-2 border-gray-900 bg-gray-50  px-12 py-3 text-sm font-medium text-gray-900 transition hover:bg-gray-50 focus:outline-none focus:ring focus:ring-blue-400"
+              >
+                Contact Us
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <img
+          alt=""
+          src={""}
+          className="bg-gray-400 object-cover mb-40 xl:mr-10 vh-75 xl:self-end xl:rounded-tl-[150px] xl:rounded-br-[150px]"
+        />
+      </section>
+    </>
+  );
 }
 
 export default ContactUsBanner;
--- src/Landing/Testimonial.js
+++ src/Landing/Testimonial.js
@@ -1,38 +1,117 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import "swiper/css/scrollbar";
-import { Pagination, Navigation } from "swiper";
+
+SwiperCore.use([Pagination, Navigation]);
 
 const mockData = [
   {
     id: 1,
     stars: 5,
-    date: new Date(),
+    date: new Date(2022, 2, 2),
     author: "Person Person",
     review: "It was cool yes it was so cool",
   },
   {
     id: 2,
-    stars: 5,
-    date: new Date(),
+    stars: 3,
+    date: new Date(2023, 1, 3),
     author: "Person Person",
     review: "It was cool yes it was so cool",
   },
   {
     id: 3,
-    stars: 5,
+    stars: 4,
     date: new Date(),
     author: "Person Person",
     review: "It was cool yes it was so cool",
   },
 ];
 
-function Testimonial(props) {
+function Testimonial() {
+  const renderStars = (count) => {
+    const stars = [];
+    for (let i = 0; i < count; i++) {
+      stars.push(
+        <span key={i} className="text-yellow-500">
+          ★
+        </span>
+      );
+    }
+    return stars;
+  };
+
+  const formatReviewDate = (date) => {
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const diffInTime = today.getTime() - date.getTime();
+    const diffInDays = Math.floor(diffInTime / (1000 * 3600 * 24));
+    const diffInWeeks = Math.floor(diffInDays / 7);
+    const diffInMonths = Math.floor(diffInDays / 30);
+    const diffInYears = Math.floor(diffInDays / 365);
+
+    if (
+      date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
+    ) {
+      return (
+        <>
+          Today
+          <span className="text-sm font-semibold text-white px-2 py-1">
+            NEW
+          </span>
+        </>
+      );
+    } else if (
+      date.getDate() === yesterday.getDate() &&
+      date.getMonth() === yesterday.getMonth() &&
+      date.getFullYear() === yesterday.getFullYear()
+    ) {
+      return (
+        <>
+          Yesterday
+          <span className="text-sm font-semibold text-white px-2 py-1">
+            NEW
+          </span>
+        </>
+      );
+    } else if (diffInDays < 7) {
+      return (
+        <>
+          {diffInDays} {diffInDays === 1 ? "day" : "days"} ago
+          {diffInDays <= 7 && (
+            <span className="text-sm font-semibold text-white px-2 py-1">
+              NEW
+            </span>
+          )}
+        </>
+      );
+    } else if (diffInWeeks < 5) {
+      return (
+        <>
+          {diffInWeeks} {diffInWeeks === 1 ? "week" : "weeks"} ago
+          {diffInWeeks <= 4 && (
+            <span className="text-sm font-semibold text-white px-2 py-1">
+              NEW
+            </span>
+          )}
+        </>
+      );
+    } else if (diffInMonths < 12) {
+      return `${diffInMonths} ${diffInMonths === 1 ? "month" : "months"} ago`;
+    } else {
+      return `${diffInYears} ${diffInYears === 1 ? "year" : "years"} ago`;
+    }
+  };
+
   return (
-    <section className="photos-section flex flex-col gap-14 md:m-20 m-12">
+    <section className="photos-section flex flex-col gap-14 md:m-20 m-4">
       <h2 className="text-4xl text-center">Testimonials</h2>
       <div className="flex justify-center">
         <Swiper
@@ -43,12 +122,28 @@ function Testimonial(props) {
             clickable: true,
           }}
           slidesPerView={1}
-          modules={[Pagination, Navigation]}
-          className="h-96 object-contain w-full relative"
+          className="h-96 w-full"
+          style={{
+            "--swiper-pagination-color": "#000080",
+            "--swiper-pagination-bullet-inactive-color": "#D9D9D9",
+          }}
         >
           {mockData.map((review) => (
-            <SwiperSlide className="p-14 flex m-auto" key={review}>
-              <div>Hi!</div>
+            <SwiperSlide key={review.id}>
+              <div className="flex justify-center items-center h-full">
+                <div className="bg-[#01519B] text-white p-6 rounded-3xl gap-8 rounded-br-[75px] flex flex-col items-center md:w-1/2 w-full m-auto h-2/3">
+                  <div className="flex gap-4">
+                    <div className="flex items-center mb-4">
+                      {renderStars(review.stars)}
+                    </div>
+                    <div className="mb-4">{formatReviewDate(review.date)}</div>
+                  </div>
+                  <div className="text-center">
+                    <p className="text-xl">{review.review}</p>
+                    <div className="text-xl">-{review.author}</div>
+                  </div>
+                </div>
+              </div>
             </SwiperSlide>
           ))}
         </Swiper>
